4cded313aeea1624f9d37e0b4df60c15b8cbc114
Paul Hayes <fofr@users.noreply.github.com>
2017-04-29 13:05:44 +0100

    Stub org page and projects
    * Use fake data
    * Include some margin styles
    * Ignore public/ directory where built files are put

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
+var globals = function(req, res, next) {
+  res.locals.globals = {
+    organisation: 'AlphaOrg',
+    projects: [
+      {
+        title: 'New navigation',
+        slug: 'new-navigation',
+        desc: 'Testing new navigation with users'
+      },
+      {
+        title: 'Search with filters',
+        slug: 'search-with-filters',
+        desc: 'Exploring problems with filtering our search'
+      },
+      {
+        title: 'Benchmarks',
+        slug: 'benchmarks',
+        desc: 'Regular research rounds covering common tasks'
+      }
+    ]
+  };
+
+  return next();
+};
+
+router.get('/', globals, function (req, res) {
   res.render('index');
+});
+
+router.get('/projects', globals, function (req, res) {
+  res.render('projects');
 });
 
 // add your routes here
